2f3d59bc625b6328cb3a5436ff22a7e80a69305a
MXCAT_PC <mxcat.xxxtremxx@gmail.com>
2018-03-21 23:31:05 -0500

    Nuevo progra calculadora inicios

---
+++ PROGRAMAS/calculadora4op.py
@@ -0,0 +1,8 @@
+print ('BIENVENIDOS A LA CALCULADORA'
+       '\nPODRA REALIZAR SOLO 4 TIPOS DE OPERACION\n'
+       '------------------------------------------\n'
+       'Elija un de las siguientes operaqciones a realizar\n'
+       'A) Suma\n'
+       'B) Resta\n'
+       'C) Multiplicacion\n'
+       'D) Divición')
