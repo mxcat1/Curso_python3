64cb0a461eb5c225d478a4922d3c6c16890df10b
MXCAT_PC <mxcat.xxxtremxx@gmail.com>
2018-03-22 19:28:27 -0500

    Añadido funcione de suma y resta

--- PROGRAMAS/calculadora4op.py
+++ PROGRAMAS/calculadora4op.py
@@ -1,3 +1,19 @@
+
+def sumar(datos):
+       suma = 0
+       for x in datos:
+              suma += x
+
+       return suma
+def restar(datos):
+       resta = 0
+       for x in datos:
+              if resta == 0:
+                     resta = x
+              else:
+                     resta -= x
+
+       return resta
 print ('BIENVENIDOS A LA CALCULADORA'
        '\nPODRA REALIZAR SOLO 4 TIPOS DE OPERACION\n'
        '------------------------------------------\n'
@@ -5,4 +21,40 @@ print ('BIENVENIDOS A LA CALCULADORA'
        'A) Suma\n'
        'B) Resta\n'
        'C) Multiplicacion\n'
-       'D) Divición')
+       'D) Divición\n'
+       '---------------------------------------------------\n'
+       'Escoge un opción escribiendo una de las letras')
+
+opcion=input(">")
+
+print('indique la cantidad de numeros para efectuar la operación')
+
+cantim = int(input('> '))
+
+if cantim >= 2:
+       if opcion == 'A':
+              x=0
+              dato = []
+              while x < cantim:
+
+                     datos = int(input("INGRESE EL "+ str(x + 1)+ " NUMERO PARA SUMAR---> "))
+                     dato.append(datos)
+                     x += 1
+
+              operaciosuma = sumar(dato)
+              print("LA SUMA ES = " , str(operaciosuma))
+       elif opcion == 'B':
+              x=0
+              dato = []
+              while x < cantim:
+                     datos = int(input("INGRESE EL "+ str(x + 1)+ " NUMERO PARA RESTAR --------> "))
+                     dato.append(datos)
+                     x +=1
+              operacioresta = restar(dato)
+              print("LA RESTA ES = ", str(operacioresta))
+
+
+
+
+
+
