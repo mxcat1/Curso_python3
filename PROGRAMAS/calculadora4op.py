c0610ce9afed71924912eaaedfc7ca80beeabbb3
MXCAT_PC <mxcat.xxxtremxx@gmail.com>
2018-03-23 21:26:11 -0500

    terminado opcion de usar recursividad en la funciones

--- PROGRAMAS/calculadora4op.py
+++ PROGRAMAS/calculadora4op.py
@@ -14,6 +14,28 @@ def restar(datos):
                      resta -= x
 
        return resta
+
+def multiplicacion(datos):
+       multi = 0
+       for x in datos:
+              if multi == 0:
+                     multi = x
+              else:
+                     multi *= x
+
+       return multi
+
+
+def division(datos):
+       dividir = 0
+       for x in datos:
+              if dividir == 0:
+                     dividir = x
+              else:
+                     dividir /= x
+
+       return dividir
+
 print ('BIENVENIDOS A LA CALCULADORA'
        '\nPODRA REALIZAR SOLO 4 TIPOS DE OPERACION\n'
        '------------------------------------------\n'
@@ -52,6 +74,25 @@ if cantim >= 2:
                      x +=1
               operacioresta = restar(dato)
               print("LA RESTA ES = ", str(operacioresta))
+
+       elif opcion == 'C':
+              x=0
+              dato = []
+              while  x< cantim:
+                     datos = int(input("INGRESE EL "+ str(x + 1)+"NUMERO PARA MULTIPLICAR -------->"))
+                     dato.append(datos)
+                     x +=1
+              operacionmulti = multiplicacion(dato)
+              print("LA MULTIPLICACION ES = ", str(operacionmulti))
+       elif opcion == 'D':
+              x = 0
+              dato = []
+              while x < cantim:
+                     datos = int(input("INGRESE EL " + str(x + 1) + "NUMERO PARA DIVIDIR -------->"))
+                     dato.append(datos)
+                     x += 1
+              operaciondivi = division(dato)
+              print("LA DIVISION ES = ", str(operaciondivi))
 
 
 
